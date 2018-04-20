18863b6d42291fdc9e4da024f5da2736c27ba7a7
SandersBP <belliveau-pattern-s@webmail.uwinnipeg.ca>
2018-04-20 15:05:03 -0500

    Adding Random Scene loading

--- 5 Second Game/Assets/Scripts/SceneManagement.cs
+++ 5 Second Game/Assets/Scripts/SceneManagement.cs
@@ -8,13 +8,24 @@ using UnityEngine.SceneManagement;
  Description: Loads the scenes n shit
 */
 
+
+
 public class SceneManagement : MonoBehaviour {
+
+    private ArrayList gameScenes;
+    
+    public void GetGameScenes()
+    {
+        
+        Debug.Log(SceneManager.GetSceneByPath("Assets/_Scenes/GameScenes"));
+    }
 
     //loads a scene using a random array of the scenes
 	public void LoadGameScene(string name)
     {
         Debug.Log("Scene loaded was : " + name);
         SceneManager.LoadScene(name);
+        GetGameScenes();
     }
 
     //will only work in production. Wont work in editor
