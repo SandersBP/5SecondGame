eb9b299aed0cae648783182c59a84ce04a45d047
SandersBP <belliveau-pattern-s@webmail.uwinnipeg.ca>
2018-04-20 13:54:29 -0500

    Added Scene Manager

---
+++ 5 Second Game/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**  
+ Author: Sanders
+ Description: Loads the scenes n shit
+*/
+
+public class SceneManagement : MonoBehaviour {
+
+    //loads a scene using a random array of the scenes
+	public void LoadGameScene(string name)
+    {
+        Debug.Log("Scene loaded was : " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    //will only work in production. Wont work in editor
+    public void QuitRequest()
+    {
+        Application.Quit();
+    }
+
+}
