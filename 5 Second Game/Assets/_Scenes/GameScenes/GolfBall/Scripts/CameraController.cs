d4e9d4afbd9df77ca678dca0ef58c4ae55521aa3
SandersBP <belliveau-pattern-s@webmail.uwinnipeg.ca>
2018-04-22 17:46:22 -0500

    Golf Game Almost done & probuilder

--- 5 Second Game/Assets/_Scenes/GameScenes/GolfBall/Scripts/CameraController.cs
+++ 5 Second Game/Assets/_Scenes/GameScenes/GolfBall/Scripts/CameraController.cs
@@ -5,12 +5,12 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;
-
+    
     private Vector3 offset;
 
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;    
     }
 
     void LateUpdate()
