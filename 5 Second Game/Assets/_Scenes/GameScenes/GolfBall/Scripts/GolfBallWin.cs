acbc51537566e3779944b4918fcf9209b5067231
SandersBP <belliveau-pattern-s@webmail.uwinnipeg.ca>
2018-04-22 20:02:24 -0500

    Golf Ball Complete
    Need to add a win condition and fix the scene manager

---
+++ 5 Second Game/Assets/_Scenes/GameScenes/GolfBall/Scripts/GolfBallWin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBallWin : MonoBehaviour {
+
+    public void collide()
+    {
+        SceneManagement.Win();
+    }
+}
